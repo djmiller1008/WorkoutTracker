cf1e0faf782a839bb638fef5999cff57d269a6e6
David Miller <djmiller1008@yahoo.com>
2022-04-27 21:47:59 +0800

    added session route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    before_validation :ensure_session_token!
+    before_validation :ensure_session_token
 
     validates :username, presence: true, uniqueness: true
     validates :password_digest, presence: true
