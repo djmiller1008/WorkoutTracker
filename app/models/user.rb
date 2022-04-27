d6f2e296e552db405a306deb8f0a38c559f02f2e
David Miller <djmiller1008@yahoo.com>
2022-04-27 20:13:44 +0800

    added find user method and session-token methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
     validates :password, length: {minimum: 6}, allow_nil: true
     attr_reader :password
 
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
+    end
+
     def password=(password)
         self.password_digest = BCrypt::Password.create(password)
     end
@@ -13,7 +19,14 @@ class User < ApplicationRecord
         BCrypt::Password.new(self.password_digest).is_password?(password)
     end
 
-    
+    def ensure_session_token 
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
 
+    def reset_session_token
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save
+        self.session_token
+    end
 
 end
