f2eb7eb7ef3d363f09ce8c24a5af53df62ea20ed
David Miller <djmiller1008@yahoo.com>
2022-04-27 20:05:49 +0800

    added user validations, password methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,18 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :password_digest, presence: true
     validates :session_token, presence: true, uniqueness: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+    attr_reader :password
 
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    
 
 
 end
