01a372120ed9c54008d9cd0349ff5416fd394e54
David Miller <djmiller1008@yahoo.com>
2022-04-27 19:49:29 +0800

    added user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :session_token, presence: true, uniqueness: true
+
 
 
 end
