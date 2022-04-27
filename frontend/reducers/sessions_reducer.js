f77138650876d967818a85b07420ddf50f7442a8
David Miller <djmiller1008@yahoo.com>
2022-04-27 22:27:15 +0800

    added users, entities reducers

--- frontend/reducers/sessions_reducer.js
+++ frontend/reducers/sessions_reducer.js
@@ -2,13 +2,17 @@ import { RECEIVE_CURRENT_USER,
          LOGOUT_USER
 } from "../actions/session_actions";    
 
-const sessionsReducer = (state = {}, action) => {
+const defaultState = {
+    current_user_id: null
+};
+
+const sessionsReducer = (state = defaultState, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            return Object.assign({}, state, action.user);
+            return {current_user_id: action.user.id}
         case LOGOUT_USER:
-            return Object.assign({}, state);
+            return {current_user_id: null};
         default: 
             return state;
     };
