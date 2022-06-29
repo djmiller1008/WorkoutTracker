0241fe2f337db99d030da14f5cfb9158f60b99f0
David <djmiller1008@yahoo.com>
2022-06-29 23:17:15 +0800

    Add update endpoint to users route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "root#root"
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :show, :destroy]
+    resources :users, only: [:create, :show, :destroy, :update]
     resource :session, only: [:create, :destroy]
     resources :workouts, only: [:index, :create, :show, :destroy] do
       resources :workout_logs, only: [:index, :create, :show, :destroy, :update]
