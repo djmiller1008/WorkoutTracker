46265e0c041c092c635176255d585004a1156ed0
David <djmiller1008@yahoo.com>
2022-06-29 23:17:58 +0800

    Add update method to user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,11 +3,26 @@ class Api::UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            login!(@user)
             render :show
+            login!(@user)
         else
             render json: @user.errors.full_messages, status: 401
         end
+    end
+
+    def update
+        @user = User.find_by(id: params[:id])
+        
+        if params[:data][:email]
+            if @user.update_attributes({ email: params[:data][:email] })
+                render :show
+            else
+                render json: @user.errors.full_messages, status: 422
+            end
+        elsif params[:data][:password]
+
+        end
+     
     end
 
     private
