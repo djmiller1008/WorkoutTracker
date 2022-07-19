c4d4453e2513deadcd117de0ff64a2c5388550fc
David <djmiller1008@yahoo.com>
2022-07-19 20:52:32 +0800

    Add action creators for user exercises

--- frontend/actions/user_exercise_actions.js
+++ frontend/actions/user_exercise_actions.js
@@ -1,6 +1,12 @@
 import * as APIUtil from "../util/user_exercise_api_util";
 
 export const RECEIVE_USER_EXERCISE = "RECEIVE_USER_EXERCISE";
+export const RECEIVE_USER_EXERCISES = "RECEIVE_USER_EXERCISES"
+
+export const receiveUserExercises = exercises => ({
+    type: RECEIVE_USER_EXERCISES,
+    exercises
+});
 
 export const receiveUserExercise = exercise => ({
     type: RECEIVE_USER_EXERCISE,
@@ -10,4 +16,9 @@ export const receiveUserExercise = exercise => ({
 export const createUserExercise = exercise => dispatch => (
     APIUtil.createUserExercise(exercise)
         .then(exercise => dispatch(receiveUserExercise(exercise)))
+);
+
+export const fetchUserExercises = () => dispatch => (
+    APIUtil.fetchUserExercises()
+        .then(exercises => dispatch(receiveUserExercises(exercises)))
 );
