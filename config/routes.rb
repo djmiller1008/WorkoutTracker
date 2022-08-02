69422648e6ad71519c0690e1fe0682f505c1a9dc
David <djmiller1008@yahoo.com>
2022-08-02 16:19:35 +0800

    Add routes for nutrition controllers

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
     resources :exercises, only: [:create, :show, :destroy, :index]
     resources :exercise_categories, only: [:index, :show]
     resources :user_exercises, only: [:create, :show, :destroy, :index]
+    resources :food_items, only: [:create, :destroy, :index, :show]
+    resources :food_diaries, only: [:create, :destroy, :index, :show]
+    resources :food_logs, only: [:create, :destroy, :index, :show]
   end
   
 end
