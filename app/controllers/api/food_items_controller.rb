8b59e25325f6418c4e8f7b4c3fd0213dea9b3a52
David <djmiller1008@yahoo.com>
2022-08-02 21:25:03 +0800

    Add food items controller, and create and index methods

---
+++ app/controllers/api/food_items_controller.rb
@@ -0,0 +1,26 @@
+class Api::FoodItemsController < ApplicationController
+
+    def index
+        @food_items = current_user.food_items
+        render :index
+    end
+
+    def create
+        @food_item = FoodItem.new(food_item_params)
+        if @food_item.save
+            render :index
+        else
+            render json: ["Invalid Entry"], status: 401
+        end
+    end
+
+    def destroy
+    end
+
+    private
+
+    def food_item_params
+        params.require(:food_item).permit(:user_id, :name, :calories, :fat, :protein, :carbohydrates)
+    end
+
+end
