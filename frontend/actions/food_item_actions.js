c5324a99ecc121e6773be3dd6c3c54887ed20e4d
David <djmiller1008@yahoo.com>
2022-08-04 17:25:09 +0800

    Add fetch food item constant, action, and thunk action creator

--- frontend/actions/food_item_actions.js
+++ frontend/actions/food_item_actions.js
@@ -1,14 +1,25 @@
 import * as APIUtil from "../util/food_item_api_util";
 
 export const RECEIVE_ALL_FOOD_ITEMS = "RECEIVE_ALL_FOOD_ITEMS";
+export const RECEIVE_FOOD_ITEM = "RECEIVE_FOOD_ITEM";
+
+export const receiveFoodItem = foodItem => ({
+    type: RECEIVE_FOOD_ITEM,
+    foodItem
+});
 
 export const receiveAllFoodItems = foodItems => ({
     type: RECEIVE_ALL_FOOD_ITEMS,
     foodItems
 });
 
-export const fetchAllFoodItems = () => dispatch => {
+export const fetchFoodItem = id => dispatch => (
+    APIUtil.fetchFoodItem(id)
+        .then(foodItem => dispatch(receiveFoodItem(foodItem)))
+);
+
+export const fetchAllFoodItems = () => dispatch => (
     APIUtil.fetchAllFoodItems()
         .then(foodItems => dispatch(receiveAllFoodItems(foodItems)))
-};
+);
 
