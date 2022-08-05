351f98dbd0dbd6f95f1e196fbc489c5c7491682f
David <djmiller1008@yahoo.com>
2022-08-05 22:57:45 +0800

    Fix typo

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,7 +1,7 @@
 class FoodItem < ApplicationRecord
     validates :user_id, presence: true
     validates :name, presence: true
-    validates :calores, presence: true
+    validates :calories, presence: true
 
     belongs_to :user
 
