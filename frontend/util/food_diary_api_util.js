beb3aa46e9f71c02918afa9cc6efab512139d593
David <djmiller1008@yahoo.com>
2022-08-07 11:57:00 +0800

    Add food diary ajax requests

---
+++ frontend/util/food_diary_api_util.js
@@ -0,0 +1,22 @@
+export const createFoodDiary = food_diary => (
+    $.ajax({
+        method: "POST",
+        url: "/api/food_diaries",
+        data: { food_diary }
+    })
+);
+
+export const fetchFoodDiaries = limit => (
+    $.ajax({
+        method: "GET",
+        url: "/api/food_diaries",
+        data: { limit }
+    })
+);
+
+export const fetchFoodDiary = id => (
+    $.ajax({
+        method: "GET",
+        url: `/api/food_diaries/${id}`
+    })
+);
