44c733989df2ebf83e8bd1569d64b563d428de45
David <djmiller1008@yahoo.com>
2022-08-07 12:01:44 +0800

    Add food diary table rendering and create new food diary button

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import DashnavContainer from './dashnav_container';
 import WorkoutItem from "../workouts/workout_item";
+import FoodDiaryItem from "../nutrition/food_diary_item";
 
 
-const Dashboard = ({ workouts, fetchWorkouts }) => {
+const Dashboard = ({ workouts, foodDiaries, fetchWorkouts, fetchFoodDiaries }) => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -12,33 +13,58 @@ const Dashboard = ({ workouts, fetchWorkouts }) => {
             fetchWorkouts(10).then(() => setLoading(false));
         }, 500);
         return () => clearTimeout(timer);
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        let timer = setTimeout(() => {
+            fetchFoodDiaries(10);
+        }, 500);
+        return () => clearTimeout(timer);
+    }, []);
 
     const renderWorkouts = () => {
         let display = [];
         if (workouts.length > 0 && !loading) {
-            workouts.slice(0, 10).forEach((workout, i) => {
+            workouts.forEach((workout, i) => {
                 const workoutElement = <WorkoutItem key={i} workout={workout} />
                 display.push(workoutElement);
             })
             return (
-                <section className="workout-log-table">
+                <section className="dashboard-content-table">
                     {display}
                 </section>
             )
         } else if (loading) {
-            return <div className="no-workouts-div">Loading...</div>
+            return <div className="no-content-div">Loading...</div>
         } else {
-            return <div className="no-workouts-div">You Don't Have Any Workouts!</div>
+            return <div className="no-content-div">You Don't Have Any Workouts!</div>
         }
     };
+
+    const renderFoodLogs = () => {
+        let display = [];
+        if (foodDiaries.length > 0 && !loading) {
+            foodDiaries.forEach((foodDiary, i) => {
+                const foodDiaryElement = <FoodDiaryItem key={i} foodDiary={foodDiary} />
+                display.push(foodDiaryElement);
+            })
+            return (
+                <section className="dashboard-content-table">
+                    {display}
+                </section>
+            )
+        } else if (loading) {
+            return <div className="no-content-div">Loading...</div>
+        }
+        return <div className="no-content-div">You Don't Have Any Entries!</div>
+    }
 
     return (
         <div>
             <DashnavContainer />
           
             <div className="dashboard-content-div">
-                <div className="my-workouts-div">
+                <div className="content-box-div">
                     <h1>My Workout Log</h1>
                     
                     {renderWorkouts()}
@@ -47,8 +73,13 @@ const Dashboard = ({ workouts, fetchWorkouts }) => {
                     </div>
                    
                 </div>
-                <div className="new-workout-link-div">
+                <div className="content-box-div">
+                    <h1>My Food Diary</h1>
+                    {renderFoodLogs()}
+                </div>
+                <div className="dashboard-link-div">
                     <Link className="dashboard-link" to="./workouts/new">Log A New Workout</Link>
+                    <Link className="dashboard-link" to="./food_diary/new">Create a Food Diary Entry</Link>
                 </div>
             </div>
         </div>
--- frontend/components/dashboard/dashboard_container.js
+++ frontend/components/dashboard/dashboard_container.js
@@ -2,15 +2,18 @@ import { connect } from "react-redux";
 import Dashboard from "./dashboard";
 import { fetchWorkouts } from "../../actions/workout_actions";
 import { sortByDate } from "../../reducers/selectors";
+import { fetchFoodDiaries } from "../../actions/food_diary_actions";
 
 const mapStateToProps = ({ entities }) => {
     return ({
-        workouts: sortByDate(entities.workouts)
+        workouts: sortByDate(entities.workouts),
+        foodDiaries: sortByDate(entities.foodDiaries)
     });
 };
 
 const mapDispatchToProps = dispatch => ({
-    fetchWorkouts: (data) => dispatch(fetchWorkouts(data))
+    fetchWorkouts: (data) => dispatch(fetchWorkouts(data)),
+    fetchFoodDiaries: limit => dispatch(fetchFoodDiaries(limit))
 });
 
 export default connect(
