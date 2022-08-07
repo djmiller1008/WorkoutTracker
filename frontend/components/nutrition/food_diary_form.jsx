52236f34d958f981ee3dd7d0b19973830c13fc18
David <djmiller1008@yahoo.com>
2022-08-07 11:58:42 +0800

    Add form to create a new food diary entry

---
+++ frontend/components/nutrition/food_diary_form.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import DashNavLink from "../dashboard/dashnavlink";
+
+const FoodDiaryForm = ({ userId, createFoodDiary }) => {
+
+    const [formData, setFormData] = useState({
+        user_id: userId,
+        date: ''
+    });
+
+    const handleInput = property => {
+        return event => setFormData({...formData, [property]: event.target.value})
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        createFoodDiary(formData)
+    }
+
+
+
+    return (
+        <div>
+            <DashNavLink />
+            <div className="new-form-div">
+                <h1>Pick a Day for your Food Diary</h1>
+                <form onSubmit={handleSubmit} className='new-content-form'>
+                    <input className="date-input" type='date'
+                            onChange={handleInput('date')} />
+                    <button className="create-button">Create Food Diary</button>
+                </form>
+            </div>
+        </div>
+    )
+
+};
+
+export default FoodDiaryForm;
