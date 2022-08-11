3aa2aec499956bb05fdda6f43e4fda3dd011f4bc
David <djmiller1008@yahoo.com>
2022-08-11 23:36:25 +0800

    Insert food item id into food log item before creation

--- app/controllers/api/food_logs_controller.rb
+++ app/controllers/api/food_logs_controller.rb
@@ -2,8 +2,10 @@ class Api::FoodLogsController < ApplicationController
 
     def create
         @food_log = FoodLog.new(food_log_params)
+        food_item_id = FoodItem.find_by(name: params[:food_log][:name]).id
+        @food_log.food_item_id = food_item_id
         if @food_log.save
-            @food_logs = FoodDiary.find_by(@food_log.food_diary_id).food_logs
+            @food_logs = FoodDiary.find_by(id: @food_log.food_diary_id).food_logs
             render :index
         else
             render json: @food_log.errors.full_messages, status: 401
@@ -19,7 +21,7 @@ class Api::FoodLogsController < ApplicationController
     private
 
     def food_log_params
-        params.require(:food_log).permit(:food_item_id, :user_id, :servings, :food_diary_id)
+        params.require(:food_log).permit(:food_item_id, :user_id, :servings, :food_diary_id, :name)
     end
 
 
