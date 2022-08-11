c752ff3f79d621f4b8a00cd4847fb051c4ce88f7
David <djmiller1008@yahoo.com>
2022-08-11 23:38:05 +0800

    Implement food log form, including search functionality

--- frontend/components/nutrition/food_log_form.jsx
+++ frontend/components/nutrition/food_log_form.jsx
@@ -1,18 +1,98 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import DashNavLink from "../dashboard/dashnavlink";
 
-const FoodLogForm = ({ createFoodLog, userId }) => {
+const FoodLogForm = ({ fetchAllFoodItems, createFoodLog, userId, foodItems }) => {
+    const params = useParams();
+    const history = useHistory();
+
     const [formData, setFormData] = useState({
         food_item_id: '',
         user_id: userId,
         servings: '',
-        food_diary_id: '',
+        food_diary_id: params.foodDiaryId,
         name: ''
-    })
+    });
+
+    useEffect(() => {
+        fetchAllFoodItems()
+    }, []);
+
+   
+    const handleInput = property => {
+        return event => setFormData({...formData, [property]: event.target.value})
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        createFoodLog(formData)
+            .then(() => history.replace(`/food_diaries/${params.foodDiaryId}`))
+    };
+
+    const selectFoodItemName = name => {
+        event.preventDefault();
+        return () => setFormData({...formData, name: name});
+    };
+
+    const matches = () => {
+        let matches = [];
+        let found = false;
+        if (formData.name.length === 0) {
+            return matches;
+        }  
+
+        Object.values(foodItems).forEach(foodItem => {
+            let partialName = foodItem.name.slice(0, formData.name.length);
+            if (formData.name === foodItem.name) {
+                found = true;
+                return;
+            } else if (partialName.toLowerCase() === formData.name.toLowerCase()) {
+                matches.push(foodItem.name);
+            }
+        })
+
+        if (matches.length === 0 && found === false) {
+            matches.push('No Food Items Found');
+            return matches;
+        }
+
+        return matches;
+    }
+
+    const possibleFoodItems = matches().map((match, i) => {
+        if (match === 'No Food Items Found') {
+            return <span>{match}</span>
+        } else {
+            return <li onClick={selectFoodItemName(match)} key={i}>{match}</li>
+        }
+    });
+  
 
     return (
         <div>
-            hello
+            <DashNavLink />
+            <div className="workout-log-main-form-div">
+                <form className="workout-log-form">
+                    <div className="exercise-search">
+                        <label>Food Item:</label>
+                        <input type='text' value={formData.name}
+                                onChange={handleInput('name')}
+                        />
+                    </div>
+                    <ul className="possible-exercises">
+                        {possibleFoodItems}
+                    </ul>
+
+                    <section className="sets-section">
+                        <label>Servings:</label>
+                        <input type='number' min='1'
+                                onChange={handleInput('servings')}
+                        />
+                    </section>
+                    
+                    <button onClick={handleSubmit} className='log-submit'>Add Food Log</button>
+                </form>
+            </div>
         </div>
     )
 
--- frontend/components/nutrition/food_log_form_container.js
+++ frontend/components/nutrition/food_log_form_container.js
@@ -1,13 +1,16 @@
 import { connect } from "react-redux";
 import { createFoodLog } from "../../actions/food_log_actions";
+import { fetchAllFoodItems } from "../../actions/food_item_actions";
 import FoodLogForm from "./food_log_form";
 
-const mapStateToProps = ({ session }) => ({
-    userId: session.id
+const mapStateToProps = ({ session, entities }) => ({
+    userId: session.id,
+    foodItems: entities.foodItems
 });
 
 const mapDispatchToProps = dispatch => ({
-    createFoodLog: foodLog => dispatch(createFoodLog(foodLog))
+    createFoodLog: foodLog => dispatch(createFoodLog(foodLog)),
+    fetchAllFoodItems: () => dispatch(fetchAllFoodItems())
 });
 
 export default connect(
