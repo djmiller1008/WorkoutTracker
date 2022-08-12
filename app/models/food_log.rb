e88c47e9caea6f57dafaa445225e5f093a398aa2
David <djmiller1008@yahoo.com>
2022-08-12 22:58:54 +0800

    Add class method to calculate nutrient totals

--- app/models/food_log.rb
+++ app/models/food_log.rb
@@ -8,6 +8,23 @@ class FoodLog < ApplicationRecord
 
     belongs_to :user
     belongs_to :food_diary
+    belongs_to :food_item
 
+    def self.find_nutrient_totals(food_logs)
+        nutrient_info = {
+            calories: 0,
+            fat: 0,
+            carbohydrates: 0,
+            protein: 0
+        }
+
+        food_logs.each do |food_log|
+            nutrient_info[:calories] += (food_log.food_item.calories * food_log.servings)
+            nutrient_info[:protein] += (food_log.food_item.protein * food_log.servings)
+            nutrient_info[:carbohydrates] += (food_log.food_item.carbohydrates * food_log.servings)
+            nutrient_info[:fat] += (food_log.food_item.fat * food_log.servings)
+        end
+        nutrient_info
+    end
 
 end
