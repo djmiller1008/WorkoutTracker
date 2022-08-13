33969acae535fcb77d90b7427f131b4b4da54005
David <djmiller1008@yahoo.com>
2022-08-13 23:06:50 +0800

    Add error rendering

--- frontend/components/nutrition/food_diary_form.jsx
+++ frontend/components/nutrition/food_diary_form.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-
 import DashNavLink from "../dashboard/dashnavlink";
 
-const FoodDiaryForm = ({ userId, createFoodDiary }) => {
+const FoodDiaryForm = ({ userId, createFoodDiary, errors, clearErrors }) => {
     const history = useHistory();
 
     const [formData, setFormData] = useState({
@@ -11,8 +11,12 @@ const FoodDiaryForm = ({ userId, createFoodDiary }) => {
         date: ''
     });
 
+    useEffect(() => {
+        clearErrors();
+    }, []);
+
     const handleInput = property => {
-        return event => setFormData({...formData, [property]: event.target.value})
+        return event => setFormData({...formData, [property]: event.target.value});
     };
 
     const handleSubmit = event => {
@@ -21,12 +25,20 @@ const FoodDiaryForm = ({ userId, createFoodDiary }) => {
             .then(foodDiary => history.push(`/food_diaries/${Object.values(foodDiary.diary)[0].id}`))
     };
 
-
+    const renderErrors = () => {
+        if (errors.length > 0) {
+            return errors.map((err, i) => <p key={i}>{err}</p>)
+        }
+    }
 
     return (
         <div>
             <DashNavLink />
+            <div className="form-errors-div">
+                {renderErrors()}
+            </div>
             <div className="new-form-div">
+                
                 <h1>Pick a Day for your Food Diary</h1>
                 <form onSubmit={handleSubmit} className='new-content-form'>
                     <input className="date-input" type='date'
