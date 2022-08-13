f86824ab2d16e7cca1870d4d5138d4997b190dd0
David <djmiller1008@yahoo.com>
2022-08-13 23:07:13 +0800

    Add error rendering function to container

--- frontend/components/nutrition/food_diary_form_container.js
+++ frontend/components/nutrition/food_diary_form_container.js
@@ -1,13 +1,15 @@
 import { connect } from "react-redux";
 import FoodDiaryForm from "./food_diary_form";
-import { createFoodDiary } from "../../actions/food_diary_actions";
+import { clearFoodDiaryErrors, createFoodDiary } from "../../actions/food_diary_actions";
 
 const mapStateToProps = state => ({
-    userId: state.session.id
+    userId: state.session.id,
+    errors: state.errors.foodDiary
 });
 
 const mapDispatchToProps = dispatch => ({
-    createFoodDiary: diary => dispatch(createFoodDiary(diary))
+    createFoodDiary: diary => dispatch(createFoodDiary(diary)),
+    clearErrors: () => dispatch(clearFoodDiaryErrors())
 });
 
 export default connect(
