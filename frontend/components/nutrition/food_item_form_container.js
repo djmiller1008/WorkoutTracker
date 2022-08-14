4709080dcd20b8b3f94cc4c3eba27532ec15e9dc
David <djmiller1008@yahoo.com>
2022-08-14 14:54:01 +0800

    Connect clear errors, create a food item, and errors state object to the food item form component

--- frontend/components/nutrition/food_item_form_container.js
+++ frontend/components/nutrition/food_item_form_container.js
@@ -1,13 +1,15 @@
 import { connect } from "react-redux";
 import FoodItemForm from "./food_item_form";
-import { createFoodItem } from "../../actions/food_item_actions";
+import { clearFoodItemErrors, createFoodItem } from "../../actions/food_item_actions";
 
-const mapStateToProps = ({ session }) => ({
-    userId: session.id
+const mapStateToProps = ({ session, errors }) => ({
+    userId: session.id,
+    errors: errors.foodItem
 });
 
 const mapDispatchToProps = dispatch => ({
-    createFoodItem: (foodItem) => dispatch(createFoodItem(foodItem))
+    createFoodItem: (foodItem) => dispatch(createFoodItem(foodItem)),
+    clearErrors: () => dispatch(clearFoodItemErrors())
 });
 
 export default connect(
