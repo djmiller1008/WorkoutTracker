33a71d46c3aff080a5d9156c6a368797914000fd
David <djmiller1008@yahoo.com>
2022-08-14 14:37:54 +0800

    Add food items, food logs errors reducer to error reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -3,12 +3,16 @@ import sessionErrorsReducer from "./session_errors_reducer";
 import workoutErrorsReducer from "./workout_errors_reducer";
 import accountUpdateErrorsReducer from "./account_update_errors";
 import foodDiaryErrorsReducer from "./food_diary_errors_reducer";
+import foodLogErrorsReducer from "./food_log_errors_reducer";
+import foodItemErrorsReducer from "./food_items_errors_reducer";
 
 const errorsReducer = combineReducers({
     session: sessionErrorsReducer,
     workout: workoutErrorsReducer,
     account: accountUpdateErrorsReducer,
-    foodDiary: foodDiaryErrorsReducer
+    foodDiary: foodDiaryErrorsReducer,
+    foodLog: foodLogErrorsReducer,
+    foodItem: foodItemErrorsReducer
 });
 
 export default errorsReducer;
