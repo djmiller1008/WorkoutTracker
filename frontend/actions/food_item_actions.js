4ece8649a93a1921e8e5c8b35770f93ee8aa001e
David <djmiller1008@yahoo.com>
2022-08-14 14:51:12 +0800

    Add actions to receive food item errors and clear the errors

--- frontend/actions/food_item_actions.js
+++ frontend/actions/food_item_actions.js
@@ -3,6 +3,17 @@ import * as APIUtil from "../util/food_item_api_util";
 export const RECEIVE_ALL_FOOD_ITEMS = "RECEIVE_ALL_FOOD_ITEMS";
 export const RECEIVE_FOOD_ITEM = "RECEIVE_FOOD_ITEM";
 export const CREATE_FOOD_ITEM = "CREATE_FOOD_ITEM";
+export const RECEIVE_FOOD_ITEM_ERRORS = "RECEIVE_FOOD_ITEM_ERRORS";
+export const CLEAR_FOOD_ITEM_ERRORS = "CLEAR_FOOD_ITEM_ERRORS";
+
+export const receiveFoodItemErrors = errors => ({
+    type: RECEIVE_FOOD_ITEM_ERRORS,
+    errors
+});
+
+export const clearFoodItemErrors = () => ({
+    type: CLEAR_FOOD_ITEM_ERRORS
+});
 
 export const receiveFoodItem = foodItem => ({
     type: RECEIVE_FOOD_ITEM,
@@ -24,11 +35,11 @@ export const fetchAllFoodItems = () => dispatch => (
         .then(foodItems => dispatch(receiveAllFoodItems(foodItems)))
 );
 
-export const createFoodItem = foodItem => dispatch => {
-    return (
+export const createFoodItem = foodItem => dispatch => (
     APIUtil.createFoodItem(foodItem)
-        .then(foodItems => dispatch(receiveAllFoodItems(foodItems))))
-};
+        .then(foodItems => dispatch(receiveAllFoodItems(foodItems)))
+        .fail(errors => dispatch(receiveFoodItemErrors(errors.responseJSON)))
+);
 
 export const deleteFoodItem = id => dispatch => (
     APIUtil.deleteFoodItem(id)
