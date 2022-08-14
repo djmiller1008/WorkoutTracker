b2a012c865e7a5025d7e409277f306d4ba3e426c
David <djmiller1008@yahoo.com>
2022-08-14 14:51:37 +0800

    Add actions to receive food log errors and clear the errors

--- frontend/actions/food_log_actions.js
+++ frontend/actions/food_log_actions.js
@@ -2,6 +2,17 @@ import * as APIUtil from '../util/food_log_api_util';
 
 export const RECEIVE_FOOD_LOGS = "RECEIVE_FOOD_LOGS";
 export const RECEIVE_FOOD_LOG = "RECEIVE_FOOD_LOG";
+export const RECEIVE_FOOD_LOG_ERRORS = "RECEIVE_FOOD_LOG_ERRORS";
+export const CLEAR_FOOD_LOG_ERRORS = "CLEAR_FOOD_LOG_ERRORS";
+
+export const clearFoodLogErrors = () => ({
+    type: CLEAR_FOOD_LOG_ERRORS
+});
+
+export const receiveFoodLogErrors = errors => ({
+    type: RECEIVE_FOOD_LOG_ERRORS,
+    errors
+});
 
 export const receiveFoodLogs = foodLogs => ({
     type: RECEIVE_FOOD_LOGS,
@@ -16,6 +27,7 @@ export const receiveFoodLog = foodLog => ({
 export const createFoodLog = foodLog => dispatch => (
     APIUtil.createFoodLog(foodLog)
         .then(foodLogs => dispatch(receiveFoodLogs(foodLogs)))
+        .fail(errors => dispatch(receiveFoodLogErrors(errors.responseJSON)))
 );
 
 export const fetchFoodLogs = foodDiaryId => dispatch => (
