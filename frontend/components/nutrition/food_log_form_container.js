18f0ac1db938d2629061885bb7564658cdcc9639
David <djmiller1008@yahoo.com>
2022-08-14 14:54:51 +0800

    Connect errors state object, clear errors function to food log form component

--- frontend/components/nutrition/food_log_form_container.js
+++ frontend/components/nutrition/food_log_form_container.js
@@ -1,16 +1,18 @@
 import { connect } from "react-redux";
-import { createFoodLog } from "../../actions/food_log_actions";
+import { clearFoodLogErrors, createFoodLog } from "../../actions/food_log_actions";
 import { fetchAllFoodItems } from "../../actions/food_item_actions";
 import FoodLogForm from "./food_log_form";
 
-const mapStateToProps = ({ session, entities }) => ({
+const mapStateToProps = ({ session, entities, errors }) => ({
     userId: session.id,
-    foodItems: entities.foodItems
+    foodItems: entities.foodItems,
+    errors: errors.foodLog
 });
 
 const mapDispatchToProps = dispatch => ({
     createFoodLog: foodLog => dispatch(createFoodLog(foodLog)),
-    fetchAllFoodItems: () => dispatch(fetchAllFoodItems())
+    fetchAllFoodItems: () => dispatch(fetchAllFoodItems()),
+    clearErrors: () => dispatch(clearFoodLogErrors())
 });
 
 export default connect(
