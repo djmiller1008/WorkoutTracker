43e35364eb3fea96a88118abae5a951350b39528
David <djmiller1008@yahoo.com>
2022-12-24 11:22:07 +0800

    Add more test coverage for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,10 +26,63 @@ RSpec.describe User, type: :model do
     end
 
     it "ensures a new user enters a unique email" do
-        user = FactoryBot.build(:user)
-        user.save()
+        FactoryBot.create(:user)
         expect(User.create(email: 'david@david.com', password: 'password')).to_not be_valid
     end 
+
+    describe 'find_by_credentials' do
+        context 'with valid params' do
+            it 'returns the user' do
+                user = FactoryBot.create(:user)
+                result = User.find_by_credentials(user.email, user.password) 
+                expect(result).to eq(user)
+            end
+        end
+
+        context 'with invalid params' do 
+            it 'returns nil' do
+                result = User.find_by_credentials('bad_email', 'bad_password')
+                expect(result).to eq(nil)
+            end
+        end
+
+        context 'with valid email and invalid password' do
+            it 'returns nil' do
+                user = FactoryBot.create(:user)
+                result = User.find_by_credentials(user.email, 'bad_password')
+                expect(result).to eq(nil)
+            end
+        end
+    end
+
+    describe 'reset_session_token!' do
+        it 'resets the users session token' do
+            user = FactoryBot.create(:user)
+            session_token = user.session_token
+            new_session_token = user.reset_session_token!
+            expect(new_session_token).to_not eq(session_token)
+        end
+    end
+
+    describe 'show_food_diaries' do
+        context 'when no limit is passed as an argument' do
+            it 'returns all food_diaries' do 
+                user = FactoryBot.create(:user)
+                FactoryBot.create_list(:food_diary, 15, user: user)
+                food_diaries = user.show_food_diaries
+                expect(food_diaries.length).to eq(15)
+            end
+        end
+
+        context 'when a limit is passed as an argument' do
+            it 'returns a limited number of food diaries' do
+                user = FactoryBot.create(:user) 
+                FactoryBot.create_list(:food_diary, 10, user: user)
+                food_diaries = user.show_food_diaries(5)
+                expect(food_diaries.length).to eq(5)
+            end
+        end
+    end
 
 
 end
