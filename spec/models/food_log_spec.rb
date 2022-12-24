b2e5e8dbf6d6296237be87162b24adc445829263
David <djmiller1008@yahoo.com>
2022-12-24 11:22:45 +0800

    Add more test coverage for food log model

--- spec/models/food_log_spec.rb
+++ spec/models/food_log_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe FoodLog, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:food_diary) }
   it { should belong_to(:food_item) }
+
+  describe 'find_nutrient_totals' do 
+    it 'calculates and returns nutrient totals of given food logs' do
+      food_logs = FactoryBot.build_list(:food_log, 10)
+     
+      result = FoodLog.find_nutrient_totals(food_logs)
+      expect(result).to eq({:calories=>16500, :fat=>540, :carbohydrates=>1320, :protein=>750})
+    end
+  end
 end
