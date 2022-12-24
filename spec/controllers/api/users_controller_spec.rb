42474cad6abb81f5aa0a06c4f0d18b5973076755
David <djmiller1008@yahoo.com>
2022-12-24 14:22:25 +0800

    Add tests for update function of users controller

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -16,6 +16,38 @@ RSpec.describe Api::UsersController, type: :controller do
             end
         end
     end
+
+    describe 'PUT #update' do
+        context 'as a normal user' do
+            it 'correctly updates the users email' do
+                @current_user = User.find_by(email: 'david@david.com')
+                controller.stub(:current_user).and_return(@current_user)
+                put :update, params: { id: @current_user.id, data: {email: 'bob@bob.com'} }, format: :json
+                expect(User.find_by(email: 'bob@bob.com')).to_not eq(nil)
+                expect(User.find_by(email: 'david@david.com')).to eq(nil)
+            end
+        end 
+
+        context 'as an admin/demo' do
+            it 'does not update the email, renders an error message' do
+                @current_user = User.create(email: 'admin@admin.com', password: 'password')
+                controller.stub(:current_user).and_return(@current_user)
+                response = put :update, params: { id: @current_user.id, data: {email: 'bob@bob.com'} }, format: :json
+                expect(response.status).to eq(401)
+                expect(response.body).to eq("[\"Unauthorized to change admin email\"]")
+            end
+        end
+
+        context 'with an invalid id' do
+            it 'returns an error' do 
+                @current_user = FactoryBot.build(:user)
+                controller.stub(:current_user).and_return(@current_user)
+                response = put :update, params: { id: 999, data: {email: 'm@m.com'} }, format: :json
+                expect(response.status).to eq(401)
+                expect(response.body).to eq("[\"User not found\"]")
+            end
+        end
+    end
 end
 
 
