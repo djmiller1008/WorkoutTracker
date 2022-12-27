efd59f777f58172892a31d2456dd0cee4450ab44
David <djmiller1008@yahoo.com>
2022-12-27 15:35:22 +0800

    Parse response bodies for easier equality testing

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -28,13 +28,14 @@ RSpec.describe Api::UsersController, type: :controller do
             end
         end 
 
-        context 'as an admin/demo' do
+        context 'as an admin' do
             it 'does not update the email, renders an error message' do
                 @current_user = User.create(email: 'admin@admin.com', password: 'password')
                 controller.stub(:current_user).and_return(@current_user)
                 response = put :update, params: { id: @current_user.id, data: {email: 'bob@bob.com'} }, format: :json
                 expect(response.status).to eq(401)
-                expect(response.body).to eq("[\"Unauthorized to change admin email\"]")
+                message = JSON.parse(response.body)[0]
+                expect(message).to eq("Unauthorized to change admin email")
             end
         end
 
@@ -44,7 +45,8 @@ RSpec.describe Api::UsersController, type: :controller do
                 controller.stub(:current_user).and_return(@current_user)
                 response = put :update, params: { id: 999, data: {email: 'm@m.com'} }, format: :json
                 expect(response.status).to eq(401)
-                expect(response.body).to eq("[\"User not found\"]")
+                message = JSON.parse(response.body)[0]
+                expect(message).to eq("User not found")
             end
         end
     end
