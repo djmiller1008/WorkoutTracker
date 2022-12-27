5303308892fea26ffca2a5a6403fc35be2404e69
David <djmiller1008@yahoo.com>
2022-12-27 15:35:49 +0800

    Add testing for workouts controller

--- spec/controllers/api/workouts_controller_spec.rb
+++ spec/controllers/api/workouts_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::WorkoutsController, type: :controller do
+    render_views
     describe 'POST #create' do
         context 'with invalid params' do
             it 'validates the presence of user_id' do
@@ -11,8 +12,12 @@ RSpec.describe Api::WorkoutsController, type: :controller do
 
         context 'with valid params' do
             it 'returns json' do
-                post :create, params: { workout: {user_id: 1, date: '11/11/2011'} }, format: :json
+                user = FactoryBot.create(:user)
+                post :create, params: { workout: {user_id: user.id, date: "2022-11-22"} }, format: :json
                 expect(response.content_type).to eq("application/json")
+                expect(response.status).to eq(200)
+                workout = Workout.find_by(user_id: user.id)
+                expect(workout).to_not eq(nil)
             end
         end
     end
@@ -26,5 +31,35 @@ RSpec.describe Api::WorkoutsController, type: :controller do
         end
     end
 
+    describe 'GET #show' do
+        context 'with a valid workout id' do
+            it 'returns workout as json' do
+                workout = FactoryBot.create(:workout)
+                get :show, params: {id: workout.id}, format: :json
+                expect(response.status).to eq(200)
+                body = JSON.parse(response.body)
+                expect(body[workout.id.to_s]["date"]).to eq("11/22/2022")
+            end
+        end
 
+        context 'with an invalid workout id' do
+            it 'returns an error message' do
+                get :show, params: {id: 'bad_id'}, format: :json
+                message = JSON.parse(response.body)[0]
+                expect(response.status).to eq(401)
+                expect(message).to eq("This workout doesn't exist!")
+            end
+        end
+    end
+
+    describe 'DELETE #destroy' do
+        it 'deletes the workout from the database' do
+            workout = FactoryBot.create(:workout)
+            current_user = workout.user 
+            controller.stub(:current_user).and_return(current_user)
+            delete :destroy, params: {id: workout.id}, format: :json
+            workout = Workout.find_by(id: workout.id)
+            expect(workout).to eq(nil)
+        end
+    end
 end
