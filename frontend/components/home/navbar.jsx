a8481e7b74d005f4cff9efa4e0a32245bc053e72
David <djmiller1008@yahoo.com>
2023-04-29 00:27:48 +0800

    Remove about link, make title a link back to home page

--- frontend/components/home/navbar.jsx
+++ frontend/components/home/navbar.jsx
@@ -5,14 +5,15 @@ import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 
 const Navbar = () => (
-    <header className="header">
+    <header className="dashboard-header header">
         <nav className="nav-bar">
             <div className="title-div">
-                <FontAwesomeIcon className="logo" icon={solid('dumbbell')} />
-                <h1 className="title">RepLogger</h1>  
-            </div>
+                <Link className="dashboard-title-link" to="/">
+                    <FontAwesomeIcon className="logo" icon={solid('dumbbell')} />
+                    <h1 className="title">RepLogger</h1> 
+                </Link>
+            </div> 
             <div className="info-div">
-                <a className="about-link" href="#"></a>
                 <div className="nav-links-div">
                     <Link to="./login">Login</Link> 
                     <Link to="./signup">Register</Link>  
