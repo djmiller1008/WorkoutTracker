3f5f3a399f0d01cbe66984c3011510719a4ef07c
David <djmiller1008@yahoo.com>
2023-04-29 00:26:23 +0800

    Refactoring, add dashnav

--- frontend/components/nutrition/food_item_form.jsx
+++ frontend/components/nutrition/food_item_form.jsx
@@ -1,8 +1,6 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import DashNavLink from "../dashboard/dashnavlink";
+import DashnavContainer from "../dashboard/dashnav_container";
 
 const FoodItemForm = ({ createFoodItem, userId, errors, clearErrors }) => {
     const history = useHistory();
@@ -20,14 +18,12 @@ const FoodItemForm = ({ createFoodItem, userId, errors, clearErrors }) => {
         clearErrors();
     }, []);
 
-
     const handleInput = property => {
         return event => setFormData({...formData, [property]: event.target.value })
     };
 
     const handleSubmit = event => {
         event.preventDefault();
-
         createFoodItem(formData)
             .then(() => history.replace('/nutrition/items/all'))
     };
@@ -39,53 +35,54 @@ const FoodItemForm = ({ createFoodItem, userId, errors, clearErrors }) => {
     }
 
     return (
-        <div>
-            <DashNavLink />
-            <div className="form-errors-div">
-                {renderErrors()}
+        <>
+            <DashnavContainer />
+            <div className="subpage-container">
+                <h1 className="subpage-h1">Create a Food Item</h1>
+                <div className="form-errors-div">
+                    {renderErrors()}
+                </div>
+                <form className="food-item-form">
+
+                    <section className="food-item-form-section">
+                        <label>Food Item Name</label>
+                        <input className="food-item-name-input" 
+                                type='text'
+                                onChange={handleInput('name')} />
+                    </section>
+
+                    <section className="food-item-form-section">
+                        <label>Calories</label>
+                        <input className="food-item-number-input"
+                                type='number'
+                                onChange={handleInput('calories')} />
+                    </section>
+
+                    <section className="food-item-form-section">
+                        <label>Fat (g)</label>
+                        <input className="food-item-number-input"
+                                type='number'
+                                onChange={handleInput('fat')} />
+                    </section>
+
+                    <section className="food-item-form-section">
+                        <label>Carbohydrates (g)</label>
+                        <input className="food-item-number-input"
+                                type='number'
+                                onChange={handleInput('carbohydrates')} />
+                    </section>
+
+                    <section className="food-item-form-section">
+                        <label>Protein (g)</label>
+                        <input className="food-item-number-input"
+                                type='number'
+                                onChange={handleInput('protein')} />
+                    </section>
+
+                    <button onClick={handleSubmit} className="create-button">Add Food Item</button>
+                </form>
             </div>
-            <h1 className="subpage-h1">Create a Food Item</h1>
-            
-            <form className="food-item-form">
-
-                <section className="food-item-form-section">
-                    <label>Food Item Name</label>
-                    <input className="food-item-name-input" 
-                            type='text'
-                            onChange={handleInput('name')} />
-                </section>
-
-                <section className="food-item-form-section">
-                    <label>Calories</label>
-                    <input className="food-item-number-input"
-                            type='number'
-                            onChange={handleInput('calories')} />
-                </section>
-
-                <section className="food-item-form-section">
-                    <label>Fat (g)</label>
-                    <input className="food-item-number-input"
-                            type='number'
-                            onChange={handleInput('fat')} />
-                </section>
-
-                <section className="food-item-form-section">
-                    <label>Carbohydrates (g)</label>
-                    <input className="food-item-number-input"
-                            type='number'
-                            onChange={handleInput('carbohydrates')} />
-                </section>
-
-                <section className="food-item-form-section">
-                    <label>Protein (g)</label>
-                    <input className="food-item-number-input"
-                             type='number'
-                             onChange={handleInput('protein')} />
-                </section>
-
-                <button onClick={handleSubmit} className="create-button">Add Food Item</button>
-            </form>
-        </div>
+        </>
     )
 
 };
