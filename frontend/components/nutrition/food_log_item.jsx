96686e4bffb925df51597aede73edf5d8a7b47b3
David <djmiller1008@yahoo.com>
2023-05-01 17:38:45 +0800

    Add calories to each foodlog

--- frontend/components/nutrition/food_log_item.jsx
+++ frontend/components/nutrition/food_log_item.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
 const FoodLogItem = ({ foodLog }) => {
-
     return (
-        <div className="workout-log-main-content">
-            <div className="single-food-log-item">
-                <h2>{foodLog.name}</h2>
-                <section className="servings-section">
+        <div className="single-food-log-item">
+            <h2>{foodLog.name}</h2>
+            <div className="food-log-subinfo-div">
+                <section>
                     Servings: {foodLog.servings}
+                </section>
+                <section>
+                    Calories: {foodLog.calories}
                 </section>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default FoodLogItem;
