9881ef180536120b47a844bb9483e2fc264ff5b3
David <djmiller1008@yahoo.com>
2023-06-12 17:35:58 +0800

    Add weight unit to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_01_090341) do
+ActiveRecord::Schema.define(version: 2023_06_12_085746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2023_05_01_090341) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", null: false
+    t.string "weight_unit", default: "kg"
   end
 
   create_table "workout_logs", force: :cascade do |t|
