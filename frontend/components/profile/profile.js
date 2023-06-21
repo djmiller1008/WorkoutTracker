455e91f9bb530384337c4c81d8c54e631ae423c2
David <djmiller1008@yahoo.com>
2023-06-21 13:29:25 -0400

    Add section where user can choose their preferred weight unit

--- frontend/components/profile/profile.js
+++ frontend/components/profile/profile.js
@@ -2,7 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import DashnavContainer from "../dashboard/dashnav_container";
 
-const Profile = ({ currentUser }) => {
+const Profile = ({ currentUser, updateWeightUnit }) => {
+
+    const handleUpdateWeightUnit = e => {
+        const selected = e.target.value;
+        updateWeightUnit(selected);
+    }
+
     return (
         <>
             <DashnavContainer />
@@ -12,6 +18,21 @@ const Profile = ({ currentUser }) => {
                     <ul className="account-links-ul">
                         <li><Link to="/profile/email">Change Email</Link></li>
                     </ul>
+                    <h1 className="preferences-h1">Preferences</h1>
+                    <form onChange={handleUpdateWeightUnit} className="weight-unit-preference-form">
+                        <input name="weight_unit" id="kg" type="radio" value="kg"
+                            defaultChecked={currentUser.weight_unit === "kg" ? true : false}></input>
+                        <label className="weight-unit-label" htmlFor="kg">kg</label>
+                        <input name="weight_unit" id="lbs" type="radio" value="lbs"
+                            defaultChecked={currentUser.weight_unit === "lbs" ? true : false}></input>
+                        <label className="weight-unit-label" htmlFor="lbs">lbs</label>
+                        <input name="weight_unit" id="bw" type="radio" value="bw"
+                            defaultChecked={currentUser.weight_unit === "bw" ? true : false}></input>
+                        <label className="weight-unit-label" htmlFor="bw">bw</label>
+                        <input name="weight_unit" id="none" type="radio" value="none"
+                            defaultChecked={currentUser.weight_unit === "none" ? true : false}></input>
+                        <label className="weight-unit-label" htmlFor="none">None</label>
+                    </form>
                 </section>
                 <section className="account-management-section">
                     <h1>Profile</h1>
