d6a67efdd9e90c18c536686affeb816c3b07a581
David <djmiller1008@yahoo.com>
2023-06-21 13:29:51 -0400

    Connect update weight unit action to component

--- frontend/components/profile/profile_container.js
+++ frontend/components/profile/profile_container.js
@@ -1,11 +1,16 @@
 import { connect } from "react-redux";
 import Profile from "./profile";
+import { updateWeightUnit } from "../../actions/account_actions";
 
 const mapStateToProps = ({ entities, session }) => ({
     currentUser: entities.users[session.id],
 });
 
+const mapDispatchToProps = dispatch => ({
+    updateWeightUnit: data => dispatch(updateWeightUnit(data))
+});
+
 export default connect(
     mapStateToProps,
-    null
+    mapDispatchToProps
 )(Profile);
