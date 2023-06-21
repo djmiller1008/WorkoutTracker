92025bbec95db3431bba15ce534f3d0577af1ce7
David <djmiller1008@yahoo.com>
2023-06-21 13:28:52 -0400

    Add updateWeightUnit ajax put request

--- frontend/util/account_api_util.js
+++ frontend/util/account_api_util.js
@@ -1,8 +1,18 @@
-export const updateData = (data) => {
+export const updateData = data => {
     return (
         $.ajax({
             method: 'PUT',
             url: `/api/users/${data.id}`,
+            data: { data }
+        })
+    )
+};
+
+export const updateWeightUnit = data => {
+    return (
+        $.ajax({
+            method: 'PUT',
+            url: '/api/users/update_weight_unit',
             data: { data }
         })
     )
